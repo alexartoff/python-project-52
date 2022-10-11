33122640926e741ac0e15762e104c027c8d15196
alexandertarakanov <alex.artoff@gmail.com>
2022-10-11 23:58:34 +0500

    edit allowed hosts in settings

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['webserver', '127.0.0.1']
+ALLOWED_HOSTS = ['webserver', '127.0.0.1', '.herokuapp.com', 'localhost']
 
 # Application definition
 
