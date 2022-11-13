8fb4b8b2f009789c0b7dc5007429a67f2c84e453
alexandertarakanov <alex.artoff@gmail.com>
2022-11-14 01:21:15 +0500

    settings update

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -113,7 +113,8 @@ DATABASES = {
     }
 }
 
-DATABASES['default'].update(dj_database_url.config(conn_max_age=500))
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
