04ad9043b6877860c8a9b18f172f397a65588406
alexandertarakanov <alex.artoff@gmail.com>
2022-11-25 21:58:01 +0500

    fix translate.step_03

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -111,22 +111,24 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': os.environ.get("TM_DB_NAME"),
-#         'USER': os.environ.get("TM_DB_USER"),
-#         'PASSWORD': os.environ.get("TM_DB_PASSWORD"),
-#         'HOST': os.environ.get("TM_DB_HOST"),
-#         'PORT': '',
-#     }
-# }
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if os.environ.get("LOCAL_DEBUG"):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ.get("TM_DB_NAME"),
+            'USER': os.environ.get("TM_DB_USER"),
+            'PASSWORD': os.environ.get("TM_DB_PASSWORD"),
+            'HOST': os.environ.get("TM_DB_HOST"),
+            'PORT': '',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
